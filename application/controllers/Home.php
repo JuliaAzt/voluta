1742caf301b88e0cf8307f07b75f88c42c715142
fctof <fctofani2@gmail.com>
2019-02-25 16:48:11 -0300

    Arquivos config, routes, autoload e Controller Home

---
+++ application/controllers/Home.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+    
+    // Construtor da classe
+    public function __construct(){
+        parent::__construct();
+    }
+
+    // Função que exibe a página inicial.
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+}
