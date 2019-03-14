238de385c6ed434c54dd3a46917a5bc59a3cd9f2
Julia Azevedo <jhuliabeatriz@hotmail.com>
2019-03-13 22:51:00 -0300

    [Views] navbar

---
+++ application/views/Home.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Collapsible sidebar using Bootstrap 4</title>
+
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+</head>
+<style type="text/css">
+
+.logo
+{
+    width: 5%;
+
+}
+.wrapper{
+    margin-top: 70px;
+    margin-left: 50px;
+}
+.wrapper li {
+    margin-top: 10px;
+
+}
+</style>
+<body >
+
+    <nav class="container navbar">
+        <img class="logo navbar-brand" src="<?php echo base_url('assets/logo.png') ?>" alt="logo">
+    </nav>
+        <!-- Sidebar -->
+    <div class="wrapper">
+
+            <ul class="list-unstyled components">
+               <li>
+                  <a> Início </a>
+               </li>
+               <li>
+                  <a> Serviços </a>
+               </li>
+               <li>
+                   <a>Projetos </a>
+               </li>
+               <li>
+                   <a>Orçamentos </a>
+               </li>
+               <li>
+                  <a> Equipe </a>
+               </li>
+               <li>
+                  <a> Contato </a>
+               </li>
+            </ul>
+        
+
+    </div>
+
+
+
+</body>
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+</html>
